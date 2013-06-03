417ac414986ca76eb1da2ac4c47f2a49b6e4903c
Tom Kucharski <kucharski.tom@gmail.com>
2013-06-03 17:39:14 +0200

    api bundle working in equinox

---
+++ api/src/main/java/com/adas/osgi/hello/Activator.java
@@ -0,0 +1,22 @@
+package com.adas.osgi.hello;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Tomasz Kucharski <kucharski.tom@gmail.com>
+ * @since 03.06.13 17:07
+ */
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("api started");
+
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("api stopped");
+	}
+}
---
+++ api/src/main/java/com/adas/osgi/hello/HelloService.java
@@ -0,0 +1,10 @@
+package com.adas.osgi.hello;
+
+/**
+ * @author Tomasz Kucharski <kucharski.tom@gmail.com>
+ * @since 03.06.13 17:12
+ */
+public interface HelloService {
+
+	void sayHello();
+}
